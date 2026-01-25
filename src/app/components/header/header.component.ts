759d53da1d0069f5ea9ebb288b273e7c5436fb82
Naga Phanindra Ravuri <ravurinagaphanindra@gmail.com>
2026-01-24 18:17:27 -0600

    added sign up and login compoenents

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,12 +1,16 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { SignUpComponent } from '../sign-up/sign-up.component';
+import { LoginInComponent } from '../login-in/login-in.component';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, SignUpComponent, LoginInComponent],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
-
+  showSignUp = false;
+  showLogin = false;
 }
---
+++ src/app/components/sign-up/sign-up.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-sign-up',
+  standalone: true,
+  imports: [CommonModule, FormsModule],
+  templateUrl: './sign-up.component.html',
+  styleUrl: './sign-up.component.scss'
+})
+export class SignUpComponent {
+
+}
